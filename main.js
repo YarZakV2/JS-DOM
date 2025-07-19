77fb7cd08f08c564a4e316331a10f540339c9f49
YarZakV2 <yarik116117.com@gmail.com>
2025-07-19 13:35:25 +0300

    .

--- main.js
+++ main.js
@@ -1,23 +1,73 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // 1. Кнопка і текстове поле
-  const input = document.getElementById('textInput');
-  const button = document.getElementById('btn');
+//1
+const categories = document.querySelectorAll('#categories .item');
+console.log(`У списку ${categories.length} категорії.`);
 
-  button.addEventListener('click', () => {
-    button.textContent = input.value;
-  });
-
-  // 2. Зміна src зображення
-  const image = document.getElementById('mainImage');
-  image.src = 'Vision-X-600.jpg';
-
-  // 3. Зміна href посилання та alt зображення
-  const link = document.getElementById('myLink');
-  const linkImage = document.getElementById('linkImage');
-  link.href = 'https://google.com';
-  linkImage.alt = 'Це нове зображення';
-
-  // 4. Зміна першого елемента списку
-  const firstItem = document.querySelector('ul li');
-  firstItem.textContent = 'Новий текст для першого пункту';
+categories.forEach(category => {
+  const title = category.querySelector('h2').textContent;
+  const count = category.querySelectorAll('ul li').length;
+  console.log(`Категорія: ${title}`);
+  console.log(`Кількість елементів: ${count}`);
 });
+
+//2
+const ingredients = [
+  'Картопля',
+  'Гриби',
+  'Часник',
+  'Помідори',
+  'Зелень',
+  'Приправи',
+];
+
+const list = document.getElementById('ingredients');
+
+const items = ingredients.map(item => {
+  const li = document.createElement('li');
+  li.textContent = item;
+  return li;
+});
+
+list.append(...items);
+
+//3
+
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg',
+    alt: 'Orange and White Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const gallery = document.getElementById('gallery');
+
+const markup = images.map(
+  img => `<li><img src="${img.url}" alt="${img.alt}" /></li>`
+).join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+//4
+let counterValue = 0;
+
+const valueSpan = document.getElementById('value');
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const btnIncrement = document.querySelector('[data-action="increment"]');
+
+btnDecrement.addEventListener('click', () => {
+  counterValue -= 1;
+  valueSpan.textContent = counterValue;
+});
+
+btnIncrement.addEventListener('click', () => {
+  counterValue += 1;
+  valueSpan.textContent = counterValue;
+});
+
